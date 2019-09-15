8904ebcb037d8a2d6611b2ad2702cc823b7e8bf1
Ryan K. Clark <mail.w3geek@gmail.com>
2019-09-15 18:42:36 -0400

    Set up express to listen and respond to web requests.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("Hello, world!");
+let messages = require("./resources/message-bundle.json");
+let package = require("./package.json");
+let express = require("express");
+
+let app = express();
+
+app.get("/*", function(request, response) {
+	response.send(`
+		<!DOCTYPE html>
+		<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+				<title>${package.name}-${package.version}</title>
+			</head>
+			<body>
+				<h1>${package.name}-${package.version}</h1>
+				<p>${package.description}</p>
+			</body>
+		</html>
+	`);
+});
+
+app.listen(3000, function() {
+	console.info(messages.app.listen);
+});
