305c15de28caef048b5ee38443e38c7d7a5c9d7a
Ryan K. Clark <mail.w3geek@gmail.com>
2019-09-18 23:57:26 -0400

    Created and set up a book router for book-related webpages.

--- app.js
+++ app.js
@@ -19,11 +19,13 @@ const APP_DIR = path.resolve(__dirname, SRC_DIR, "./app/");
 const VIEWS_DIR = path.resolve(__dirname, APP_DIR, "./views");
 
 const ENV_PORT = process.env.port || 3000;
+const SCHEME_QUALIFIER = "://";
 const VIEW_ENGINE = "ejs";
 
 /* @globals */
-let app = express();
 let log = debug("library:app");
+let app = express();
+let bookRouter = express.Router();
 
 app.set("view engine", VIEW_ENGINE);
 app.set("views", VIEWS_DIR);
@@ -35,6 +37,19 @@ app.use("/js", express.static(JQUERY_WEBAPP_DIR));
 app.use(morgan("dev"));
 
 /* @routes */
+bookRouter.route("/")
+	.get(function(request, response) {
+		log(chalk.green(messages.onrequest.get));
+		response.send([request.protocol, SCHEME_QUALIFIER, request.hostname, request.originalUrl].join(""));
+	});
+
+bookRouter.route("/:id")
+	.get(function(request, response) {
+		log(chalk.green(messages.onrequest.books.book.get), chalk.cyan(request.params.id));
+		response.send([request.protocol, SCHEME_QUALIFIER, request.hostname, request.originalUrl].join(""));
+	});
+
+app.use("/books", bookRouter);
 app.get("/", function(request, response) {
 	log(chalk.green(messages.onrequest.get));
 	let title = `${package.name}-${package.version}`,
@@ -46,5 +61,5 @@ app.get("/", function(request, response) {
 });
 
 app.listen(ENV_PORT, function() {
-	log(chalk.green(messages.app.listen));
+	log(chalk.green(messages.app.open), chalk.magenta(messages.app.listen), chalk.cyan(ENV_PORT));
 });
