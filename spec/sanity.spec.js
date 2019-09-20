18c1a6cc23965608fe0a4b0bc1557d75a598179f
Ryan Clark <rclark@routeone.com>
2019-09-20 17:08:40 -0400

    Set up jasmine and created a basic unit test to ensure sanity.

---
+++ spec/sanity.spec.js
@@ -0,0 +1,17 @@
+describe("Sanity Spec", function() {
+	it("expects undefined to be undefined", function() {
+		expect(undefined).toBe(undefined);
+	});
+
+	it("expects null to be null", function() {
+		expect(null).toBe(null);
+	});
+
+	it("expects true to be true", function() {
+		expect(true).toBe(true);
+	});
+
+	it("expects false to be false", function() {
+		expect(false).toBe(false);
+	});
+});
